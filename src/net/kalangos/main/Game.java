0c1b76594182759eb14a94c0cefc98d324842682
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-06-09 10:44:41 -0300

    Created class Game, folder res, sprites, floor, door, player front e back

---
+++ src/net/kalangos/main/Game.java
@@ -0,0 +1,131 @@
+package net.kalangos.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Thread thread;
+	private boolean isRunning = true;
+	public static JFrame frame;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 3;
+
+	private BufferedImage image;
+
+	
+	private BufferedImage[] player;
+	private int x = 0;
+	private int frames = 0;
+	private int maxFrames = 20;
+	private int curAnimation = 0;
+	private int maxAnimation = 3;
+
+	public Game() {
+	
+	
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	}
+
+	public void initFrame() {
+		frame = new JFrame("My Game #1");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Game game = new Game();
+		game.start();
+	}
+
+	public void tick() {
+	
+	}
+
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(0, 0, 0));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+		/* Renderização do jogo */
+		Graphics g2 = (Graphics2D) g;
+
+		/***/
+
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+
+		bs.show();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0.0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+
+		while (isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if (delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS.: " + frames);
+				frames = 0;
+				timer = System.currentTimeMillis();
+			}
+		}
+		stop();
+	}
+
+}
