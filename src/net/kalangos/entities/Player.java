4e46038050b3a8eecbbf490747d2325349a3a53c
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-06-09 22:05:34 -0300

    Movimentação do Player alterando os sprites

--- src/net/kalangos/entities/Player.java
+++ src/net/kalangos/entities/Player.java
@@ -1,31 +1,77 @@
 package net.kalangos.entities;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
-public class Player extends Entity{
-	
+import net.kalangos.main.Game;
+
+public class Player extends Entity {
+
 	public boolean right, up, left, down;
+	public int right_dir = 0;
+	public int left_dir = 1;
+	public int dir = right_dir;
 	public double speed = 1.4;
+
+	private int frames = 0, maxFrames = 5, index = 0, maxIndex = 3;
+	private boolean moved = false;
+	private BufferedImage[] rightPlayer;
+	private BufferedImage[] leftPlayer;
 
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 		// TODO Auto-generated constructor stub
+
+		rightPlayer = new BufferedImage[4];
+		leftPlayer = new BufferedImage[4];
+
+		for (int i = 0; i < 4; i++) {
+			rightPlayer[i] = Game.spritesheet.getSprite(32 + (i * 16), 0, 16, 16);
+		}
+
+		for (int i = 0; i < 4; i++) {
+			leftPlayer[i] = Game.spritesheet.getSprite(32 + (i * 16), 16, 16, 16);
+		}
 	}
-	
+
 	public void tick() {
-		if(right) {
+		moved = false;
+		if (right) {
+			moved = true;
+			dir = right_dir;
 			x += speed;
-		}else if(left) {
+		} else if (left) {
+			moved = true;
+			dir = left_dir;
 			x -= speed;
 		}
-		
-		if(up) {
+
+		if (up) {
+			moved = true;
 			y -= speed;
-		}else if(down) {
+		} else if (down) {
+			moved = true;
 			y += speed;
 		}
+		
+		if(moved) {
+			frames++;
+			if(frames == maxFrames) {
+				frames = 0;
+				index++;
+				if(index > maxIndex) {
+					index = 0;
+				}
+			}
+		}
 	}
-	
-	
+
+	public void render(Graphics g) {
+		if (dir == right_dir) {
+			g.drawImage(rightPlayer[index], this.getX(), this.getY(), null);
+		} else if (dir == left_dir) {
+			g.drawImage(leftPlayer[index], this.getX(), this.getY(), null);
+		}
+	}
 
 }
