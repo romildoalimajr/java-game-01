f00363902bf14ba77fbbd5811d89afadac382695
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-06-11 21:50:33 -0300

    Implementando o Reinicio do Game

--- src/net/kalangos/entities/Player.java
+++ src/net/kalangos/entities/Player.java
@@ -2,7 +2,9 @@ package net.kalangos.entities;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import net.kalangos.graficos.Spritesheet;
 import net.kalangos.main.Game;
 import net.kalangos.world.Camera;
 import net.kalangos.world.World;
@@ -19,17 +21,22 @@ public class Player extends Entity {
 	private boolean moved = false;
 	private BufferedImage[] rightPlayer;
 	private BufferedImage[] leftPlayer;
-	
+
+	private BufferedImage playerDamage;
+
 	public int ammo = 0;
-	
+
+	public boolean isDamaged = false;
+	private int damageFrames = 0;
+
 	public static double life = 100, maxLife = 100;
 
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
-		// TODO Auto-generated constructor stub
 
 		rightPlayer = new BufferedImage[4];
 		leftPlayer = new BufferedImage[4];
+		playerDamage = Game.spritesheet.getSprite(0, 16, 16, 16);
 
 		for (int i = 0; i < 4; i++) {
 			rightPlayer[i] = Game.spritesheet.getSprite(32 + (i * 16), 0, 16, 16);
@@ -42,60 +49,77 @@ public class Player extends Entity {
 
 	public void tick() {
 		moved = false;
-		if (right && World.isFree((int)(x + speed), this.getY())) {
+		if (right && World.isFree((int) (x + speed), this.getY())) {
 			moved = true;
 			dir = right_dir;
 			x += speed;
-		} else if (left && World.isFree((int)(x - speed), this.getY())) {
+		} else if (left && World.isFree((int) (x - speed), this.getY())) {
 			moved = true;
 			dir = left_dir;
 			x -= speed;
 		}
 
-		if (up && World.isFree(this.getX(), (int)(y - speed))) {
+		if (up && World.isFree(this.getX(), (int) (y - speed))) {
 			moved = true;
 			y -= speed;
-		} else if (down && World.isFree(this.getX(), (int)(y + speed))) {
+		} else if (down && World.isFree(this.getX(), (int) (y + speed))) {
 			moved = true;
 			y += speed;
-		}else {
-			//Estamos colidindo
-			if(Game.rand.nextInt(100) < 10) {
-				Game.player.life -= Game.rand.nextInt(5);
-				if(Game.player.life <= 0) {
-					//Game Over
-					//System.exit(1);
+		} else {
+			// Estamos colidindo
+			if (Game.rand.nextInt(100) < 10) {
+				Game.player.life -= Game.rand.nextInt(3);
+				Game.player.isDamaged = true;
+				if (Game.player.life <= 0) {
+					// Game Over
+					// System.exit(1);
 				}
-				System.out.println("Vida.: " + Game.player.life);
+				// System.out.println("Vida.: " + Game.player.life);
 			}
-			
+
 		}
-		
-		if(moved) {
-			frames++;
-			if(frames == maxFrames) {
-				frames = 0;
-				index++;
-				if(index > maxIndex) {
-					index = 0;
-				}
+
+		frames++;
+		if (frames == maxFrames) {
+			frames = 0;
+			index++;
+			if (index > maxIndex) {
+				index = 0;
 			}
 		}
-		
+
 		this.checkCollisionLifePack();
 		this.checkCollisionAmmo();
 		
-		Camera.x = Camera.clamp(this.getX() - (Game.WIDTH / 2), 0, World.WIDTH*16 - Game.WIDTH);
-		Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT / 2), 0, World.HEIGHT*16 - Game.HEIGHT);
+		if(isDamaged) {
+			this.damageFrames++;
+			if(this.damageFrames == 3) {
+				this.damageFrames = 0;
+				isDamaged = false;
+			}
+		}
+		
+		if(life <= 0) {
+			Game.entities = new ArrayList<Entity>();
+			Game.enemies = new ArrayList<Enemy>();
+			Game.spritesheet = new Spritesheet("/spritesheet.png");
+			Game.player = new Player(0, 0, 16, 16, Game.spritesheet.getSprite(32, 0, 16, 16));
+			Game.entities.add(Game.player);
+			Game.world = new World("/map.png");
+			return;
+		}
+
+		Camera.x = Camera.clamp(this.getX() - (Game.WIDTH / 2), 0, World.WIDTH * 16 - Game.WIDTH);
+		Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT / 2), 0, World.HEIGHT * 16 - Game.HEIGHT);
 	}
-	
+
 	public void checkCollisionAmmo() {
-		for(int i = 0; i < Game.entities.size(); i++) {
+		for (int i = 0; i < Game.entities.size(); i++) {
 			Entity atual = Game.entities.get(i);
-			if(atual instanceof Bullets) {
-				if(Entity.isColliding(this, atual)) {
+			if (atual instanceof Bullets) {
+				if (Entity.isColliding(this, atual)) {
 					ammo++;
-					//System.out.println("Munição atual: " + ammo);
+					// System.out.println("Munição atual: " + ammo);
 					Game.entities.remove(atual);
 				}
 			}
@@ -103,26 +127,29 @@ public class Player extends Entity {
 	}
 
 	public void checkCollisionLifePack() {
-		for(int i = 0; i < Game.entities.size(); i++) {
+		for (int i = 0; i < Game.entities.size(); i++) {
 			Entity atual = Game.entities.get(i);
-			if(atual instanceof LifePack) {
-				if(Entity.isColliding(this, atual)) {
+			if (atual instanceof LifePack) {
+				if (Entity.isColliding(this, atual)) {
 					life += 100;
-					if(life >= 100) {
+					if (life >= 100) {
 						life = 100;
 					}
 					Game.entities.remove(atual);
-					return;
 				}
 			}
 		}
 	}
-	
+
 	public void render(Graphics g) {
-		if (dir == right_dir) {
-			g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
-		} else if (dir == left_dir) {
-			g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+		if (!isDamaged) {
+			if (dir == right_dir) {
+				g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			} else if (dir == left_dir) {
+				g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			}
+		} else {
+			g.drawImage(playerDamage, this.getX() - Camera.x, this.getY() - Camera.y, null);
 		}
 	}
 
