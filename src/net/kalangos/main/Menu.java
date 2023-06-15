907647e6efa063cbfaa5f54eb772f86e943530ac
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-06-15 20:58:25 -0300

    Implementado método SaveGame e LoadGame

--- src/net/kalangos/main/Menu.java
+++ src/net/kalangos/main/Menu.java
@@ -4,7 +4,11 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -42,6 +46,37 @@ public class Menu {
 				System.exit(1);
 			}
 		}
+	}
+	
+	public static String loadGame(int encode) {
+		String line = "";
+		File file = new File("save.txt");
+		if(file.exists()) {
+			try {
+				String singleLine = null;
+				BufferedReader reader = new BufferedReader(new FileReader("save.txt"));
+				try {
+					while((singleLine = reader.readLine()) != null) {
+						String[] trans = singleLine.split(":");
+						char[] val = trans[1].toCharArray();
+						trans[1] = "";
+						for(int i = 0; i < val.length; i++) {
+							val[i] -= encode;
+							trans[1] += val[i];
+						}
+						line += trans[0];
+						line += ":";
+						line += trans[1];
+						line += "/";
+					}
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+			}catch(FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		return line;
 	}
 	
 	public static void saveGame(String[] val1, int[] val2, int encode) {
